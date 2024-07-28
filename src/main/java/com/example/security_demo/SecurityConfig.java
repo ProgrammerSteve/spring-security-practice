7df333616ff4fb111542089abb22923d95cebcb2
Steven <stevenmb1995@gmail.com>
2024-07-27 21:40:49 -0500

    added JdbcUserDetailsManager, added schema.sql, and added bcrypt for password hashing

--- src/main/java/com/example/security_demo/SecurityConfig.java
+++ src/main/java/com/example/security_demo/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.security_demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -9,8 +10,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -18,6 +24,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) ->
@@ -43,15 +53,25 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user1= User.withUsername("user1")
-                .password("{noop}test123") //{noop} prefix tells spring password should be saved as plain text
+                .password(passwordEncoder().encode("test123")) //{noop} prefix tells spring password should be saved as plain text
                 .roles("USER")
                 .build();
         UserDetails admin= User.withUsername("admin")
-                .password("{noop}admin") //{noop} prefix tells spring password should be saved as plain text
+                .password(passwordEncoder().encode("test123")) //{noop} prefix tells spring password should be saved as plain text
                 .roles("ADMIN")
                 .build();
 
+        JdbcUserDetailsManager userDetailsManager=new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
+
         //In-memory authentication
-        return new InMemoryUserDetailsManager(user1,admin);
+        //return new InMemoryUserDetailsManager(user1,admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
