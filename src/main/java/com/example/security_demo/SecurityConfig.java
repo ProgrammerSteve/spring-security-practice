f67f84fb8d89f73c20918e9fdd004b7be2ecdc6b
Steven <stevenmb1995@gmail.com>
2024-08-03 23:31:30 -0500

    edited the Security Config to add a custom security filter to the filter chain

--- src/main/java/com/example/security_demo/SecurityConfig.java
+++ src/main/java/com/example/security_demo/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.example.security_demo;
 
+import com.example.security_demo.jwt.AuthEntryPointJwt;
+import com.example.security_demo.jwt.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,6 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -25,28 +32,65 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Autowired
-    DataSource dataSource;
+    private final DataSource dataSource;
+    private final AuthEntryPointJwt unauthorizedHandler;
+
+    public SecurityConfig(DataSource dataSource, AuthEntryPointJwt authEntryPointJwt) {
+        this.dataSource = dataSource;
+        this.unauthorizedHandler = authEntryPointJwt;
+    }
+
+//    @Bean
+//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((requests) ->
+//                requests
+//                        .requestMatchers("/h2-console/**").permitAll()
+//                        .anyRequest().authenticated());
+//        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        //http.formLogin(withDefaults());
+//
+//        //For httpBasic
+//        // base64 encoded  user:password
+//        // Authorization header looks like:
+//        // Authorization: Basic YWRtaW46dGVzdDEyMw==
+//        //where YWRtaW46dGVzdDEyMw== is username:password encoded into base64
+//        //can use basic auth in the authorization tab of postman to attach the authorization header
+//        http.httpBasic(withDefaults());
+//        http.headers(headers->
+//                headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+//        http.csrf(csrf->csrf.disable());
+//        return http.build();
+//    }
+
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) ->
-                requests
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().authenticated());
-        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        //http.formLogin(withDefaults());
 
-        //For httpBasic
-        // base64 encoded  user:password
-        // Authorization header looks like:
-        // Authorization: Basic YWRtaW46dGVzdDEyMw==
-        //where YWRtaW46dGVzdDEyMw== is username:password encoded into base64
-        //can use basic auth in the authorization tab of postman to attach the authorization header
-        http.httpBasic(withDefaults());
-        http.headers(headers->
-                headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
-        http.csrf(csrf->csrf.disable());
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+             //we want to permit all endpoints that goes into h2-console
+            .requestMatchers("/h2-console/**").permitAll()
+             //we want to permit endpoint for login
+            .requestMatchers("/signin").permitAll()
+             //all other requests are authenticated
+            .anyRequest().authenticated())
+             //session will be stateless for REST apis where no session states are maintained
+             //in between requests.
+            .sessionManagement(session ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // adding exception handling mechanism
+        http.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler));
+
+        //done for h2 console
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        //done for h2 console
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        //add authTokenFilter into the filter chain before UsernamePasswordAuthenticationFilter
+        //if it is never added, it never gets executed
+        http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        //returns the filter chain
         return http.build();
     }
 
@@ -73,5 +117,15 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthTokenFilter authTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
+        return builder.getAuthenticationManager();
     }
 }
